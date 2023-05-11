1ecd3bbad28dc00112321d5ecae3559deafce83f
lag369 <lag369@drexel.edu>
2023-05-11 17:29:05 -0400

    scrolltrigger addition and udpates

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -25,6 +25,26 @@ h1Hover.addEventListener("mouseover",function(){
     heroHeaderTl.play();
 })
 
+let infoHover = document.querySelector("#info-h1");
+
+let infoHeaderTl = gsap.timeline({paused:true});
+infoHeaderTl.to("#info-h1",{duration:0.25, scale:2, color:"#fff"})
+;
+
+infoHover.addEventListener("mouseover",function(){
+    infoHeaderTl.play();
+})
+
+//scrollTrigger
+//ScrollTrigger:{
+    //trigger: "#myId",
+    //scrub:true,
+    //markers:true
+    //start: "center 70%", "(trigger element) (scroller/viewport)"
+    //end: "center 10%",
+//}
+//pin:"#box" or true
+
 function oneAnimation(){
     let tl = gsap.timeline();
     tl.from("h1",{duration:1,alpha:0, y:-100})
@@ -32,5 +52,54 @@ function oneAnimation(){
     return tl;
 }
 
+function body1Animation(){
+    let tl = gsap.timeline({
+        scrollTrigger:{
+            trigger: "#body",
+            start: "top 10%",
+            end: "bottom 70%",
+            scrub:true,
+            markers:false
+    }});
+    tl.to("#body", {duration:0.25, rotation:180}, "-=0.15")
+    ;
+
+    return tl;
+}
+
+function boxAnimation(){
+    let tl = gsap.timeline({
+        scrollTrigger:{
+            trigger: "#box",
+            toggleActions:"play reverse none",
+            start: "top center",
+            end: "bottom 10%",
+            scrub:true,
+            markers:false
+    }});
+    tl.from("#box",{duration:2, scale:3, rotation:270, x:"-=300%"})
+    ;
+
+    return tl;
+}
+
+function infoAnimation(){
+    let tl = gsap.timeline({
+        scrollTrigger:{
+            trigger: "#info-h1",
+            start: "top 50%",
+            end: "bottom 80%",
+            scrub:true,
+            markers:false
+    }});
+    tl.from("#info-p",{duration:1, scale:3, rotation:180, x:"-=300%"}, "#info")
+    ;
+
+    return tl;
+}
+
 let mainTl = gsap.timeline();
 mainTl.add(oneAnimation())
+mainTl.add(boxAnimation())
+mainTl.add(infoAnimation())
+mainTl.add(body1Animation())
